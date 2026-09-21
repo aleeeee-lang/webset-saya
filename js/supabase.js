7bfd02291d0788f3a9261c790e4070cb343b14b8
aleeeee-lang <ranggadeusqieldaus@gmail.com>
2026-09-21 11:07:44 +0700

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,12 @@
+const supabaseUrl =
+    "https://vqpxumrodadqqaxuyehy.supabase.co";
+
+const supabaseKey =
+    "sb_publishable_O9MWlDg0y7UwaAOt89j1MQ_QcKGVkII";
+
+
+const supabaseClient =
+    window.supabase.createClient(
+        supabaseUrl,
+        supabaseKey
+    );

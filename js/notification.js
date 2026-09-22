ea8ba2910055ec4d706950c60dae77a7ac6c30f5
aleeeee-lang <ranggadeusqieldaus@gmail.com>
2026-09-22 16:15:45 +0700

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -2,15 +2,5 @@ const notificationButton = document.getElementById("notificationButton");
 const notificationPanel = document.getElementById("notificationPanel");
 
 notificationButton.addEventListener("click", function () {
-
-    if (notificationPanel.classList.contains("active")) {
-
-        notificationPanel.classList.remove("active");
-
-    } else {
-
-        notificationPanel.classList.add("active");
-
-    }
-
+    notificationPanel.classList.toggle("active");
 });

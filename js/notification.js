f7f7a1e7678e0c0a941b1b6a9dcc40be6f8784a9
aleeeee-lang <ranggadeusqieldaus@gmail.com>
2026-09-22 15:31:49 +0700

    Create notification.js

---
+++ js/notification.js
@@ -0,0 +1,8 @@
+const notificationButton = document.getElementById("notificationButton");
+const notificationPanel = document.getElementById("notificationPanel");
+
+notificationButton.addEventListener("click", function () {
+
+    notificationPanel.classList.toggle("active");
+
+});
